5735499875682c4891cf5894769c89e771ce3927
Rapenelton <rapenelton@gmail.com>
2017-04-20 00:16:45 -0400

    About Page
    Added junk to the about page so it doesn’t look broken tomorrow

--- views/welcome/welcome_index.class.php
+++ views/welcome/welcome_index.class.php
@@ -30,7 +30,7 @@ class WelcomeIndex extends IndexView {
                     <a href="<?= BASE_URL ?>/account/login">Login</a>
                     <a href="account/index/">View Accounts</a>
                     <a href="user/index/">View Users</a>
-                    <a href ="about.asp">About</a>
+                    <a href ="about.html">About</a>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tellus tellus, finibus nec porta ac, mattis ac lacus. Praesent accumsan odio felis, ut consectetur odio facilisis sit amet. Suspendisse semper urna a commodo aliquet. Duis ac tortor vitae erat interdum iaculis. Donec congue vitae nisl et efficitur. Integer id justo vitae enim ultricies fermentum. Sed non arcu fringilla, imperdiet risus ut, porttitor mauris. Vivamus at elit tincidunt, vulputate odio sed, vulputate augue.</p>
                 <br>
